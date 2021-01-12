6ccf97494fc1d65378245b9acae4992d81f82152
Lee Meichin <me@mrlee.dev>
2021-01-12 21:44:10 +0000

    Flesh things out a bit more

--- lispov.el
+++ lispov.el
@@ -27,7 +27,7 @@
 ;; Haskell-based static site generator.
 ;;
 ;; In place of Pandoc, lispov instead depends upon org-mode, so all org-*
-;; features are available to you in your posts.
+;; features are available to you in your posts, as well as the rest of elisp :)
 ;;
 ;; The structure is not opinionated, thus you are required to define a structure
 ;; for your site in order for it to build. In that light, a default structure is
@@ -35,7 +35,59 @@
 
 ;;; Code:
 
+(defgroup lispov nil
+  "elisp-driven, org-based static site generator"
+  :group 'tools
+  :group 'wp
+  :link '(url-link :tag "GitHub" "https://github.com/mrleedev/lispov")
+  :link '(emacs-commentary-link :tag "Commentary" "lispov"))
 
+(defcustom lispov-cache-directory "_cache"
+  "The cached site build will be stored in this directory. 
+
+It is recommended that when you change this setting, you ignore the directory in your VCS.
+Set to `nil' to disable caching entirely."
+  :group 'lispov
+  :type 'string)
+
+
+(defcustom lispov-output-directory "_site"
+  "The output of the build will be stored in this directory.
+
+This supports TRAMP-style directory strings, if you wish to deploy directly to a remote server."
+  :group 'lispov
+  :type 'string)
+
+;;;###autoload
+(defun lispov/defsite (name)
+  "Define a static site with the given name."
+)
+
+;;;###autoload
+(defun lispov/defroute (route)
+  "Define a route matching the given directory string/glob, a route transform, and a file transform, and site.
+
+Files found within this directory will become part of the final build, subject to any
+transformations supplied. A number of in-built transformers are available:
+
+  Route transforms:
+    `\'same': Explicitly state that the matched directory structure on disk will be the same structure in the build.
+      For example, if the route is `css/*' on disk, then the output will contain a matching `css' folder.
+
+  File transforms:
+    `\'copy-files':  Matching files are copied verbatim into the build.
+
+If no route transform is supplied, it is assumed that the same directory structure that is matched will be
+copied into the build."
+  )
+
+;;;###autoload
+(defun lispov/defcontent (route)
+  "Dynamically generate a file at the given route, for the given site.
+
+Use this to generate, for example, custom index pages, RSS feeds, ATOM feeds, and so on."
+  )
+  
 
 (provide 'lispov)
 ;;; lispov.el ends here
